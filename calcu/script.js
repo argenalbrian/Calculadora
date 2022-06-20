7bf2a5cf61b95d68e52734ee96f2532bd87106e2
argenalbrian <105093637+argenalbrian@users.noreply.github.com>
2022-06-20 17:31:16 -0600

    Add files via upload

---
+++ calcu/script.js
@@ -0,0 +1,18 @@
+  
+function del(){
+    value = $('.calc-display').val();
+    $('.calc-display').val(value.substring(0, value.length-1));
+
+}
+
+function eql(){
+    $('.calc-display').val(eval($('.calc-display').val()));
+}
+
+function c(){
+    $('.calc-display').val('');
+}
+
+function insrt(num){
+    $('.calc-display').val($('.calc-display').val()+num);
+}
